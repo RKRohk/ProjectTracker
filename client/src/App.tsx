1f9c50cb4ff2bd8ec8b08c49c397b7e051707fc2
rkrohk <rkakar2000@gmail.com>
2020-11-04 10:52:29 +0530

    Initialized the client in typescript

---
+++ client/src/App.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./App.css";
+import Navbar from "./Components/Navbar";
+
+const App: React.FC = () => {
+    return <Navbar />;
+};
+
+export default App;
---
+++ client/src/Components/Navbar.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Navbar:React.FC = () => {
+    return (
+        <div>
+            <div>
+                Image Goes Here
+            </div>
+            <div>
+                Features
+            </div>
+            <div>
+                Pricing
+            </div>
+            <div>
+                Blog
+            </div>
+            <div>
+                Sign In
+            </div>
+        </div>
+    )
+}
+
+export default Navbar;
