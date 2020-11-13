5d3d2491417de2c4a107f6f9e936ebebd466222f
rkrohk <rkakar2000@gmail.com>
2020-11-13 23:29:25 +0530

    Styled the login form

--- client/src/components/Login/Login.tsx
+++ client/src/components/Login/Login.tsx
@@ -1,50 +1,60 @@
 import React from "react";
 import { Formik } from "formik";
-import { Button, FormControl, FormLabel, Input } from "@chakra-ui/core";
+import { Button, Flex, FormControl, FormLabel, Input } from "@chakra-ui/core";
 const Login: React.FC = () => {
     return (
-        <Formik
-            initialValues={{ email: "", password: "" }}
-            onSubmit={(values, props) => {
-                props.setSubmitting(true);
-                setTimeout(() => {
-                    console.log(values);
-                    props.setSubmitting(false);
-                }, 5000);
-            }}
-        >
-            {(props) => (
-                <form onSubmit={props.handleSubmit}>
-                    <FormControl>
-                        <FormLabel htmlFor="email">Email Address</FormLabel>
-                        <Input
-                            value={props.values.email}
-                            onChange={props.handleChange}
-                            type="email"
-                            id="email"
-                        ></Input>
-                    </FormControl>
-                    <FormControl>
-                        <FormLabel htmlFor="password">Password</FormLabel>
-                        <Input
-                            value={props.values.password}
-                            onChange={props.handleChange}
-                            type="password"
-                            id="password"
-                        ></Input>
-                    </FormControl>
-                    <Button
-                        type="submit"
-                        isLoading={props.isSubmitting}
-                        loadingText="Submitting"
-                        variantColor="teal"
-                        variant="outline"
-                    >
-                        Submit
-                    </Button>
-                </form>
-            )}
-        </Formik>
+        <Flex mx="auto" my={40} justify="center">
+            <Formik
+                initialValues={{ email: "", password: "" }}
+                onSubmit={(values, props) => {
+                    props.setSubmitting(true);
+                    setTimeout(() => {
+                        console.log(values);
+                        props.setSubmitting(false);
+                    }, 5000);
+                }}
+            >
+                {(props) => (
+                    <form onSubmit={props.handleSubmit}>
+                        <FormControl
+                            mx="auto"
+                            width={["90vw", "90vw", "30vw", "30vw"]}
+                        >
+                            <FormLabel htmlFor="email">Email Address</FormLabel>
+                            <Input
+                                value={props.values.email}
+                                onChange={props.handleChange}
+                                type="email"
+                                id="email"
+                            ></Input>
+                        </FormControl>
+                        <FormControl
+                            mx="auto"
+                            width={["90vw", "90vw", "30vw", "30vw"]}
+                        >
+                            <FormLabel htmlFor="password">Password</FormLabel>
+                            <Input
+                                value={props.values.password}
+                                onChange={props.handleChange}
+                                type="password"
+                                id="password"
+                            ></Input>
+                        </FormControl>
+                        <Flex justify="center" my={6}>
+                            <Button
+                                type="submit"
+                                isLoading={props.isSubmitting}
+                                loadingText="Submitting"
+                                variantColor="purple"
+                                variant="outline"
+                            >
+                                Submit
+                            </Button>
+                        </Flex>
+                    </form>
+                )}
+            </Formik>
+        </Flex>
     );
 };
 
